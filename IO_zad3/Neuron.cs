21fc77377f55865c9000b5bc8892fd8c953b74d4
Maja Skowrońska <majka.skowron@gmail.com>
2011-05-30 19:45:43 +0200

    Zrobiona siec neuronowa

---
+++ IO_zad3/Neuron.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    public class Neuron
+    {
+        double[] wagi;
+
+        public Neuron(int rozmiar)
+        {
+            this.wagi=new double[rozmiar];
+            for (int i = 0; i < wagi.Length; i++)
+            {
+                Random numer = new Random();
+                wagi[i] = numer.NextDouble();
+            }
+        }
+
+        public Neuron(double[] tablica)
+        {
+            this.wagi = tablica;
+        }
+        
+
+        public double odleglosc(double[] tablica1)
+        {
+            int wymiar = tablica1.Length;
+            double sumacalosc = 0;
+
+            for (int i = 0; i < wymiar; i++)
+            {
+                sumacalosc += Math.Pow(tablica1[i] - this.wagi[i], 2);
+            }
+
+            return Math.Sqrt(sumacalosc);
+        }
+
+        double sasiedztwo(double[] tablica1, double promien)
+        {
+            double rezultat = 0.0;
+            double odl = odleglosc(tablica1);
+
+            rezultat = Math.Exp(-(Math.Pow(odl, 2)) / (2 * Math.Pow(promien, 2)));
+
+            return rezultat;
+        }
+
+        public double ucz(double wsp_uczenia, double[] tablica)
+        {
+            double s = sasiedztwo(tablica, wsp_uczenia);
+
+            for (int i = 0; i < this.wagi.Length; i++)
+            {
+                double roznica = this.wagi[i] - tablica[i];
+                this.wagi[i] += roznica * s * wsp_uczenia;
+            }
+            return odleglosc(tablica);
+        }
+    }
+}
---
+++ IO_zad3/SOM.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace WindowsFormsApplication1
+{
+    class SOM
+    {
+        Neuron[] neurony;
+        double wsp_uczenia=0.3;
+        double promien=0.5;
+
+        SOM(int n, int nwag)
+        {
+            this.neurony = new Neuron[n];
+            for (int i=0; i<n; i++){
+                this.neurony[i]=new Neuron(nwag);
+            }
+        }
+
+        SOM(int n, int nwag, double wu, double r)
+        {
+            this.neurony = new Neuron[n];
+            for (int i = 0; i < n; i++)
+            {
+                this.neurony[i] = new Neuron(nwag);
+            }
+            this.wsp_uczenia = wu;
+            this.promien = r;
+        }
+
+        void uczsiec(double [][] dane, int epoki)
+        {
+            for (int h = 0; h < epoki; h++)
+            {
+                for (int i = 0; i < dane.Length; i++)
+                {
+                    for (int j = 0; j < this.neurony.Length; j++)
+                    {
+                        this.neurony[j].ucz(this.wsp_uczenia, dane[i]);
+                    }
+
+                }
+            }
+        }
+
+        Neuron zwyciezca(double [] dane)
+        {
+            Neuron rezultat = this.neurony[0];
+
+            for (int i = 0; i < neurony.Length; i++)
+            {
+                if (neurony[i].odleglosc(dane) < rezultat.odleglosc(dane))
+                {
+                    rezultat = neurony[i];
+                }
+            }
+
+            return rezultat;
+        }
+
+        void zapiszsiec(string nazwapliku)
+        {
+            Stream stream = null;
+            try
+            {
+                IFormatter formatter = new BinaryFormatter();
+                stream = new FileStream(nazwapliku, FileMode.Create, FileAccess.Write, FileShare.None);
+                formatter.Serialize(stream, this);
+            }
+            catch
+            {
+                // do nothing, just ignore any possible errors
+            }
+            finally
+            {
+                if (null != stream)
+                    stream.Close();
+            }
+        }
+
+        public static SOM wczytajsiec(string nazwapliku)
+        {
+            Stream stream = null;
+            SOM siec = null;
+            try
+            {
+                IFormatter formatter = new BinaryFormatter();
+                stream = new FileStream(nazwapliku, FileMode.Open, FileAccess.Read, FileShare.None);
+                siec = (SOM)formatter.Deserialize(stream);
+            }
+            catch
+            {
+                // do nothing, just ignore any possible errors
+            }
+            finally
+            {
+                if (null != stream)
+                    stream.Close();
+            }
+            return siec;
+        }
+    }
+}
